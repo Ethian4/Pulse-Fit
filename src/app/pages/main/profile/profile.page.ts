2a1de12e0ca5554113b6db6cb8850a5c513ab013
Ethian4 <Ethianmartinez@outlook.com>
2024-11-16 22:24:33 -0600

    Version 1.0

--- src/app/pages/main/profile/profile.page.ts
+++ src/app/pages/main/profile/profile.page.ts
@@ -3,7 +3,6 @@ import { User } from 'src/app/models/user.model';
 import { FirebaseService } from 'src/app/services/firebase.service';
 import { UtilsService } from 'src/app/services/utils.service';
 
-
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.page.html',
@@ -14,32 +13,34 @@ export class ProfilePage implements OnInit {
   firebaseSvc = inject(FirebaseService);
   utilsSvc = inject(UtilsService);
 
-  ngOnInit() {
-  }
+  isLoggingOut = false;  // Nuevo estado para controlar la animación de cierre de sesión
 
+  ngOnInit() {}
 
-  signOut(){
-    this.firebaseSvc.signOut();
+  async signOut() {
+    this.isLoggingOut = true; // Inicia la animación
+
+    // Espera a que la animación termine antes de cerrar sesión
+    setTimeout(() => {
+      this.firebaseSvc.signOut();
+    }, 500);  // Tiempo igual al de la animación
+
   }
 
   user(): User {
     return this.utilsSvc.getFromLocalStorage('user');
-  } 
+  }
 
   async takeImage() {
     let user = this.user();
-    let path = `users/${user.uid}`
-
+    let path = `users/${user.uid}`;
 
     const dataUrl = (await this.utilsSvc.takePicture('Imagen de Perfil')).dataUrl;
 
-    //const loading = await this.utilsSvc.loading();
-    //await loading.present();
-
     let imagePath = `${user.uid}/profile`;
-    user.image = await this.firebaseSvc.uploadImage(imagePath, dataUrl);  
+    user.image = await this.firebaseSvc.uploadImage(imagePath, dataUrl);
 
-    this.firebaseSvc.updateDocument(path, {image: user.image}).then(async => {
+    this.firebaseSvc.updateDocument(path, { image: user.image }).then(async () => {
 
       this.utilsSvc.saveInLocalStorage('user', user);
 
@@ -49,8 +50,8 @@ export class ProfilePage implements OnInit {
         color: 'success',
         position: 'middle',
         icon: 'checkmark-circle-outline'
-      })
- 
+      });
+
     }).catch(error => {
       console.log(error);
 
@@ -60,8 +61,8 @@ export class ProfilePage implements OnInit {
         color: 'primary',
         position: 'middle',
         icon: 'alert-circle-outline'
-      })
-    })
+      });
+    });
   }
 
 }
